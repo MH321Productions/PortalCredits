a612ac61924401fd7a03281853912108c0baabce
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2023-01-10 09:16:44 +0100

    Restructure WinInclude, remove NOSYSMETRICS

--- include/PortalCredits/WinInclude.hpp
+++ include/PortalCredits/WinInclude.hpp
@@ -2,48 +2,153 @@
 #if !defined(PC_WININCLUDE_HPP) && defined(PC_WINDOWS)
 #define PC_WININCLUDE_HPP
 
-#define _WIN32_WINNT 0x0601
+#if _WIN32_WINNT < 0x0601
+    #undef _WIN32_WINNT
+    #define _WIN32_WINNT 0x0601
+#endif
+
 #include <sdkddkver.h>
 
-#ifndef FULL_WINTARD
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOGDICAPMASKS
 #define NOGDICAPMASKS
-#define NOSYSMETRICS
+#endif
+
+/*#ifndef NOSYSMETRICS
+//#define NOSYSMETRICS
+#endif*/
+
+#ifndef NOMENUS
 #define NOMENUS
+#endif
+
+#ifndef NOICONS
 #define NOICONS
+#endif
+
+#ifndef NOSYSCOMMANDS
 #define NOSYSCOMMANDS
+#endif
+
+#ifndef NORASTEROPS
 #define NORASTEROPS
+#endif
+
+#ifndef OEMRESOURCE
 #define OEMRESOURCE
+#endif
+
+#ifndef NOATOM
 #define NOATOM
+#endif
+
+#ifndef NOCLIPBOARD
 #define NOCLIPBOARD
+#endif
+
+#ifndef NOCOLOR
 #define NOCOLOR
+#endif
+
+#ifndef NOCTLMGR
 #define NOCTLMGR
+#endif
+
+#ifndef NODRAWTEXT
 #define NODRAWTEXT
+#endif
+
+#ifndef NOKERNEL
 #define NOKERNEL
+#endif
+
+#ifndef NONLS
 #define NONLS
+#endif
+
+#ifndef NOMEMMGR
 #define NOMEMMGR
+#endif
+
+#ifndef NOMETAFILE
 #define NOMETAFILE
+#endif
+
+#ifndef NOOPENFILE
 #define NOOPENFILE
+#endif
+
+#ifndef NOSCROLL
 #define NOSCROLL
+#endif
+
+#ifndef NOSERVICE
 #define NOSERVICE
+#endif
+
+#ifndef NOSOUND
 #define NOSOUND
+#endif
+
+#ifndef NOTEXTMETRIC
 #define NOTEXTMETRIC
+#endif
+
+#ifndef NOWH
 #define NOWH
+#endif
+
+#ifndef NOCOMM
 #define NOCOMM
+#endif
+
+#ifndef NOKANJI
 #define NOKANJI
+#endif
+
+#ifndef NOHELP
 #define NOHELP
+#endif
+
+#ifndef NOPROFILER
 #define NOPROFILER
+#endif
+
+#ifndef NODEFERWINDOWPOS
 #define NODEFERWINDOWPOS
+#endif
+
+#ifndef NOMCX
 #define NOMCX
+#endif
+
+#ifndef NORPC
 #define NORPC
+#endif
+
+#ifndef NOPROXYSTUB
 #define NOPROXYSTUB
+#endif
+
+#ifndef NOIMAGE
 #define NOIMAGE
+#endif
+
+#ifndef NOTAPE
 #define NOTAPE
 #endif
 
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 
+#ifndef STRICT
 #define STRICT
+#endif
+
 
 #include <Windows.h>
 
