d6ddf48e4d8f1fc29e6b09ebe4749bbd24831eeb
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2023-01-17 13:07:07 +0100

    Start app through main method, register signal handlers to catch kill signals and safely quit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <csignal>
+
+#include "PortalCredits/App.hpp"
 
 using namespace std;
 
-using rgb = unsigned char;
+PortalCredits::PortalCreditsApp* runningApp = NULL;
+
+void quitAppIfNecessary(int signal);
 
 int main(int anzahl, char* args[]) {
     cout << "Hello World" << endl;
+
+    //Register signal handlers
+    std::signal(SIGABRT, quitAppIfNecessary);
+    std::signal(SIGINT, quitAppIfNecessary);
+    std::signal(SIGTERM, quitAppIfNecessary);
+
+    //Run app
+    PortalCredits::PortalCreditsApp app;
+    runningApp = &app;
+    int exitCode = app.onExecute();
+
+    //Clean up
+    runningApp = NULL;
+}
+
+void quitAppIfNecessary(int signal) {
+    if (runningApp) {
+        runningApp->quit();
+        runningApp = NULL;
+    }
 }
